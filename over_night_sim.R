279ea45816c5bcb64d776f05c46fe290dce9a6f3
kasperniss01 <kasperniss@gmail.com>
2025-10-21 11:51:27 +0200

    removed files without .rds. and added those files again

--- over_night_sim.R
+++ over_night_sim.R
@@ -13,7 +13,7 @@ n500 <- T500 / L
 
 message("T: ", T500)
 df_T500 <- sim_rej_rate(T500, n500, L, B, A, seq(0.01, 1, 0.01))
-saveRDS(df_T500, file = "T500")
+saveRDS(df_T500, file = "T500.rds")
 
 ### ------------------ ####
 T2K <- 2000
@@ -21,7 +21,7 @@ n2K <- T2K / L
 
 message("T: ", T2K)
 df_T2K <- sim_rej_rate(T2K, n2K, L, B, A, seq(0.01, 1, 0.01))
-saveRDS(df_T2K, file = "T2K")
+saveRDS(df_T2K, file = "T2K.rds")
 
 ### ------------------ ####
 T5K <- 5000
@@ -29,7 +29,7 @@ n5K <- T5K / L
 
 message("T: ", T5K)
 df_T5K <- sim_rej_rate(T5K, n5K, L, B, A, seq(0.01, 1, 0.01))
-saveRDS(df_T5K, file = "T5K")
+saveRDS(df_T5K, file = "T5K.rds")
 
 ### ------------------ ####
 #note: only ran with repetitions = 200 and not 500
@@ -38,7 +38,7 @@ n10K <- T10K / L
 
 message("T: ", T10K)
 df_T10K <- sim_rej_rate(T10K, n10K, L, B, A, seq(0.01, 1, 0.01), repetitions = 200)
-saveRDS(df_T10K, file = "T10K")
+saveRDS(df_T10K, file = "T10K.rds")
 
 gather_plots(df_T500)
 gather_plots(df_T2K)
