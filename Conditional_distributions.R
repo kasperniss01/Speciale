25999cf26f3db701cb40f9fbb7261243c8595d4a
kasperniss01 <kasperniss@gmail.com>
2025-10-24 08:28:29 +0200

    updated sim_rejection_rate so that it works for a general AR(1) process and updated cond_dist to include characteristic functions in high-dimension

--- Conditional_distributions.R
+++ Conditional_distributions.R
@@ -8,9 +8,9 @@
 library(expm)
 
 ## the formula for the variance of X_t is the same for every choice of Y_t
-variance_Xt <- function(a, t) {
-  #a should be a scalar in all cases
-  (1 - a^(2 * t)) / (1 - a^2) 
+variance_Xt <- function(a, t, sigma1_sq = 1) {
+  #a is a scalar
+  sigma1_sq * (1 - a^(2 * t)) / (1 - a^2) 
 }
 
 ### For Y one-dimensional
@@ -44,41 +44,62 @@ variance_conditional_Y_given_X <- function(a, b, c, t) {
 
 
 ## for d-dimensional Y
-Covariance_Xt_Yt_highdim <- function(a, b, C, t, sigma_sq = 1) {
+Covariance_Xt_Yt_highdim <- function(a, b, C, t, sigma1_sq = 1) {
   # a scalar
   #b d-dimensional vector
   #C dxd matrix
-  #returns a dx1 vector
+  #t is vector of time-inputs
+  #returns a d x length(t) matrix
+  
+  #fix sigma1_sq later...
   
   d <- nrow(C) 
   I_d <- diag(d)
+  out <- matrix(0, nrow = d, ncol = length(t))
+  b <- matrix(b, ncol = 1)
   
-  term1 <- (I_d - (a * C) %^% t) %*% solve(I_d - a * C)
-  term2 <- a^t / a * ((a^t * I_d - C %^% t) %*% solve(a * I_d - C))
-  
-  a * sigma_sq / (1 - a^2) * (term1 - term2) %*% b
+  for (i in seq_along(t)) {
+    ti <- t[i]
+    term1 <- (I_d - (a * C) %^% ti) %*% solve(I_d - a * C)
+    term2 <- a^ti / a * ((a^ti * I_d - C %^% ti) %*% solve(a * I_d - C))
+    
+    out[, i] <- as.vector(a * sigma1_sq / (1 - a^2) * (term1 - term2) %*% b)
+  }
+  return(out)
 }
 
 variance_Yt_highdim <- function(a, b, C, t, sigma_sq = 1) {
   # a scalar
   # b dx1 vector
   # C dxd matrix
+  # t vector of time-imputs
+  
+  # returns d x d x length(t) array
   
   d <- nrow(C)
   I_d <- diag(d)
-  mat <- matrix(0, nrow = d, ncol = d)
   
+  out <- array(0, dim = c(d, d, length(t)))
   bbt <- b %*% t(b)
   
-  for(i in 1:t) {
-    mat <- mat + (C %^% (i - 1)) %*% (
-      bbt * variance_Xt(a, t - i) + 
-        b %*% t(Covariance_Xt_Yt_highdim(a, b, C, t - i)) %*% t(C) + 
-        C %*% Covariance_Xt_Yt_highdim(a, b, C, t - i) %*% t(b) +
-        sigma_sq * I_d
-        ) %*% (t(C) %^% (i - 1))
+  for(i in seq_along(t)) {
+    mat <- matrix(0, nrow = d, ncol = d)
+    ti <- t[i]
+    
+    for(j in 1:ti) {
+      mat <- mat + (C %^% (j - 1)) %*% (
+        bbt * variance_Xt(a, ti - j) + 
+          b %*% t(Covariance_Xt_Yt_highdim(a, b, C, ti - j)) %*% t(C) + 
+          C %*% Covariance_Xt_Yt_highdim(a, b, C, ti - j) %*% t(b) +
+          sigma_sq * I_d
+          ) %*% (t(C) %^% (j - 1))
+    }
+    
+    out[, , i] <- mat
   }
-  return(mat)
+  
+  if(length(t) == 1) out <- out[, , 1, drop = FALSE] #don't know if we need this?
+  return(out)
 }
 
 # Fast - chat solution
@@ -177,29 +198,87 @@ char_func_cond_Y_given_X_mat <- function(a, b, c, t, x_t, u) {
 
 
 mean_conditional_Y_given_X_highdim <- function(a, b, C, t, x_t, sigma_sq = 1) {
-  cov_Xt_Yt <- Covariance_Xt_Yt_highdim(a, b, C, t, sigma_sq)
-  var_Xt <- variance_Xt(a, t)
+  #a is scalar
+  # b is d x 1 vector
+  # C is dxd matrix
+  # t is vector of time inputs
+  #x_t is vector of x to condition on same length as t
   
-  cov_Xt_Yt / var_Xt * x_t
+  # output matrix of size d x length(t)
+  # browser()
+  
+  cov_Xt_Yt <- Covariance_Xt_Yt_highdim(a, b, C, t, sigma_sq) # d x length(t)
+  var_Xt <- variance_Xt(a, t) # t x 1
+  
+  scale <- x_t / var_Xt
+  
+  out <- t( t(cov_Xt_Yt) * scale) 
+  #why transpose this many times? to make sure that scale multiplies correctly?
+  
+  return(out)
 }
 
 variance_conditional_Y_given_X_highdim <- function(a, b, C, t, sigma_sq = 1) {
-  var_Yt <- variance_Yt_highdim(a, b, C, t, sigma_sq)
-  cov_Xt_Yt <- Covariance_Xt_Yt_highdim(a, b, C, t, sigma_sq)
-  var_Xt <- variance_Xt(a, t)
+  #a is a scalar
+  # b is dx1 vector
+  # C is dxd matrix
+  # t is vector of time inputs
+  #outputs 3D array of dimension d x d x length(t)
   
-  var_Yt - (cov_Xt_Yt %*% t(cov_Xt_Yt)) / var_Xt
+  d <- nrow(C)
+  
+  var_Yt <- variance_Yt_highdim(a, b, C, t) #dxdx length(t)
+  var_Xt <- variance_Xt(a, t) # length(t) x 1
+  cov_Xt_Yt <- Covariance_Xt_Yt_highdim(a, b, C, t) # d x length(t)
+  
+  out <- array(0, dim = c(d, d, length(t)))
+  
+  # browser()
+  for (i in seq_along(t)) {
+    r <- matrix(cov_Xt_Yt[, i], ncol = 1) #d x 1
+    vx <- var_Xt[i] # scalar
+    
+    out[, , i] <- var_Yt[, , i] - (r %*% t(r)) / vx
+  }
+  
+  if(length(t) == 1) out <- out[, , 1, drop = FALSE] #don't know if this is needed
+  
+  return(out)
 }
-char_func_conditional_Y_given_X_highdim <- function(a, b, C, t, x_t, u_vec, sigma_sq = 1) {
-  # u_vec is d-dimensional vector
+
+char_func_cond_Y_given_X_highdim_mat <- function(a, b, C, t, x_t, u, sigma_sq = 1) {
+  # a is a scalar
+  # b is a dx1 vector
+  # C is a dxd matrix
+  #t is a vector of time input
+  #x_t is a sequence of x to condition on same length as t
+  # u is either a matrix of size either dx1 or B x d if mu or nu is input
   
-  mean_cond <- mean_conditional_Y_given_X_highdim(a, b, C, t, x_t, sigma_sq)
-  var_cond <- variance_conditional_Y_given_X_highdim(a, b, C, t, sigma_sq)
+  B <- nrow(u)
+  d <- length(b)
   
-  exponent <- 1i * sum(u_vec * mean_cond) - 0.5 * t(u_vec) %*% var_cond %*% u_vec
+  if(nrow(u) == d) u <- t(u) #transpose if u is a dx1 matrix - why I don't know
   
-  exp(exponent)
+  mean <- mean_conditional_Y_given_X_highdim(a, b, C, t, x_t, sigma_sq) # d x length(t)
+  variance <- variance_conditional_Y_given_X_highdim(a, b, C, t, sigma_sq) #d x d x length(t)
+  #variance could also just be a matrix I think if t has length 1?
+  
+  out <- matrix(0, nrow = length(t), ncol = B)
+  for (i in seq_along(t)) {
+    # browser()
+    mean_i <- mean[, i, drop = F] # dx1
+    variance_i <- variance[, , i] #dxd
+    linear_term <- as.vector(u %*% mean_i) #Bx1
+    quad_term <- rowSums((u %*% variance_i) * u) # Bx1 - why rowSums?
+    out[i, ] <- exp(1i * linear_term - 0.5 * quad_term) #char. function
+  }
+  
+  if (length(t) == 1) out <- out[1, , drop = FALSE] #dont know if needed
+  
+  return(out)
 }
+
+
 
 
 
--- sim_rejection_rate.R
+++ sim_rejection_rate.R
@@ -5,66 +5,76 @@ source("simulate_AR_process.R")
 source("estimate_test.R")
 source("sim_crit_value.R")
 source("oracle_statistic.R")
+source("Conditional_distributions.R")
 
-sim_rej_rate <- function(Tlen, n, L, B,
+#only for AR1 process, consider changing data-generating process to be input so it works in general
+sim_rej_rate <- function(Tlen, L, B,
                          A_matrix, 
                          alphas,
                          repetitions = 500,
+                         remainder_true_ccfs = list(true_phi = NULL, true_psi = NULL), 
                          verbose = TRUE) {
   
+  true_phi <- remainder_true_ccfs$true_phi
+  true_psi <- remainder_true_ccfs$true_psi
+  
+  d <- ncol(A_matrix) - 1
+  
   mu <- matrix(rnorm(B), ncol = 1)
-  nu <- matrix(rnorm(B), ncol = 1)   # d = 1 here
+  nu <- matrix(rnorm(B * d), ncol = d)
   K <- length(alphas)
   
   reject <- matrix(FALSE, nrow = K, ncol = repetitions,
                    dimnames = list(paste0("alpha=", alphas), NULL))
   
-  reject_true <- matrix(FALSE, nrow = K, ncol = repetitions,
-                   dimnames = list(paste0("alpha=", alphas), NULL))
+  if (!is.null(true_phi) && !is.null(true_psi)) {
+    reject_true <- matrix(FALSE, nrow = K, ncol = repetitions,
+                          dimnames = list(paste0("alpha=", alphas), NULL))
+    
+    remainders <- list()
+    S_trues <- numeric(repetitions)
+    
+    covvar_true_list <- list()
+  }
   
   S_hats <- numeric(repetitions)
-  S_trues <- numeric(repetitions)
-  
   covvar_list <- list()
-  covvar_true_list <- list()
-  
   data_list <- list()
   
   for (i in seq_len(repetitions)) {
-    data <- simulate_2D_AR1_process(Tlen, A_matrix, verbose = FALSE)
+    data <- simulate_AR_process(Tlen, A_matrix, d = d)
     data_list[[i]] <- data
     
     est <- estimate_stat(
-      data = data, n = n, L = L, B = B,
+      data = data, L = L, B = B,
       mu = mu, nu = nu,
-      remainder_true_ccfs = list(
-        true_phi = function(x, u) char_func_cond_X_next_given_X_previous_mat(A[1,1], x, u),
-        true_psi = function(x, u, t) char_func_cond_Y_given_X_mat(A[1,1], A[2,1], A[2,2], t, x, u)
-      )
+      remainder_true_ccfs = remainder_true_ccfs
     )
-    
-    # browser()
+
     S_hat <- est$S_hat
     S_hats[i] <- S_hat
-    
-    S_true <- est$S_true
-    S_trues[i] <- S_true
     
     covvar_est <- est$Covvar_Est
     covvar_list[[i]] <- covvar_est
     
-    covvar_est_true <- est$Covvar_Est_true
-    covvar_true_list[[i]] <- covvar_est_true
-    
-    # browser()
     draws <- sim_crit_draws(covvar_est)
-    draws_true <- sim_crit_draws(covvar_est_true)
-    
     crits <- crit_from_draws(draws, alphas)
-    crits_true <- crit_from_draws(draws_true, alphas)
-    
     reject[, i] <- (S_hat > crits)
-    reject_true[, i] <- (S_true > crits_true)
+    
+    ## if calculate true stuff
+    if (!is.null(true_phi) && !is.null(true_psi)) {
+      remainders[[i]] <- est$Remainders
+      
+      S_true <- est$S_true
+      S_trues[i] <- S_true
+      
+      covvar_est_true <- est$Covvar_Est_true
+      covvar_true_list[[i]] <- covvar_est_true
+    
+      draws_true <- sim_crit_draws(covvar_est_true)
+      crits_true <- crit_from_draws(draws_true, alphas)
+      reject_true[, i] <- (S_true > crits_true)
+    }
     
     #print to see how far in loop
     if (verbose && i %% 10 == 0) message(i)
@@ -72,72 +82,80 @@ sim_rej_rate <- function(Tlen, n, L, B,
   
     #calculate mean and SEs
     rates <- rowMeans(reject)
-    rates_true <- rowMeans(reject_true)
-    
     ses   <- sqrt(rates * (1 - rates) / repetitions)
-    ses_true <- sqrt(rates_true * (1 - rates_true) / repetitions)
     
     
     #store results in a dataframe with alpha, rejection rates and SEs
     rejection_rate_df <- data.frame(alpha = alphas, 
                                     rate = as.numeric(rates), 
-                                    se = as.numeric(ses),
-                                    rate_true = as.numeric(rates_true),
-                                    se_true = as.numeric(ses_true))
+                                    se = as.numeric(ses))
     
-    #store S_hat and S estimates
-    estimates <- data.frame(S_hat = S_hats, S_true = S_trues)
+    estimates <- data.frame(S_hat = S_hats)
+    covvars <- list(est = covvar_list)
     
-    #return both dataframes
-    return(list(rejection_rate_df = rejection_rate_df, 
-                estimates = estimates, 
-                covvar = list(est = covvar_list, true = covvar_true_list),
-                data = data_list))
+    
+    if (!is.null(true_phi) && !is.null(true_psi)) {
+      
+      rates_true <- rowMeans(reject_true)
+      ses_true <- sqrt(rates_true * (1 - rates_true) / repetitions)
+      
+      # append true stuff
+      rejection_rate_df <- rejection_rate_df %>% mutate(
+        rate_true = as.numeric(rates_true),
+        se_true = as.numeric(ses_true))
+      
+      estimates <- estimates %>% mutate(S_true = S_trues)
+      
+      covvars <- append(covvars, list(true = covvar_true_list))
+    }
+    
+    
+    output <- list(rejection_rate_df = rejection_rate_df,
+                   estimates = estimates,
+                   covvar = covvars,
+                   data = data_list)
+    
+    if (!is.null(true_phi) && !is.null(true_psi)) {
+      output <- append(output, list(remainders = remainders))
+    }
+    
+    return(output)
 }
 
 
 
+Tlen <- 10
+L <- 2
+A <- matrix(c(0.3, 0, 0, 0.2, 0.7, -0.4, -0.6, 0.9, 0.3), byrow = T, nrow = 3)
+B <- 2
 
-Tlen <- 5e4
-L <- 10
-n <- Tlen / L
-B <- 10
+# data_test <- simulate_AR_process(Tlen, A)
+# estimate_stat(data_test, L, B)
 
-A <- matrix(c(-0.4, 0, -0.3, 0.8), nrow = 2, byrow = T)
+test_df_highdim <- sim_rej_rate(Tlen, L, B, A, c(0.1, 0.2), repetitions = 10,
+                        remainder_true_ccfs = list(
+                                          true_phi = function(x, u) char_func_cond_X_next_given_X_previous_mat(A[1,1], x, u),
+                                          true_psi = function(x, u, t) {
+                                            char_func_cond_Y_given_X_highdim_mat(A[1, 1], A[-1, 1], A[-1, -1], t, x, u)
+                                          }
+                                        ))
 
-df <- sim_rej_rate(Tlen, n, L, B, A, seq(0.01, 1, 0.01), repetitions = 200)
+A_small <- matrix(c(0.3, 0, -0.2, 0.7), byrow = T, nrow = 2)
+test_df_1D <- sim_rej_rate(Tlen, L, B, A_small, c(0.1, 0.2), repetitions = 10,
+                           remainder_true_ccfs = list(
+                             true_phi = function(x, u) char_func_cond_X_next_given_X_previous_mat(A[1,1], x, u),
+                             true_psi = function(x, u, t) {
+                               char_func_cond_Y_given_X_highdim_mat(A[1, 1], A[-1, 1], A[-1, -1], t, x, u)
+                             }
+                           ))
 
-# df_prec <- sim_rej_rate(Tlen, n, L, B, A, seq(0.01, 1, 0.01))
+
+
+# true_psi(10, c(1,1), 1) %>% drop()
 # 
-# df_prec_CI <- df_prec %>% mutate(lower = rate - 1.96 * se,
-#                                  upper = rate + 1.96 * se)
-# 
-# 
-# 
-ggplot(df$rejection_rate_df, aes(x = alpha, y = rate_true)) +
-  geom_line() +
-  geom_abline(color = "red") +
-  theme_bw()
-# 
-# ggplot(df_prec$estimates) + 
-#   geom_point(aes(x = S_hat, y = S)) + 
-#   geom_abline(color = "red") + 
-#   theme_bw()
-# 
-# ggplot(df_prec$estimates %>% pivot_longer(cols = c(S_hat, S))) + 
-#   geom_histogram(aes(x = value), color = "white") + 
-#   facet_wrap(~name) + 
-#   theme_bw()
-# 
-# 
-# ggplot(df_prec$estimates) + 
-#   geom_qq(aes(sample = S_hat),
-#           distribution = function(p) quantile(df_prec$estimates$S, p)) + 
-#   geom_abline(color = "red") + 
-#   theme_bw()
-# 
-# 
-# saveRDS(df_prec, file = "rej_rate_T1000.rds")
+# char_func_conditional_Y_given_X_highdim(A[1,1], A[-1,1], A[-1,-1], 10, 1, c(0.5, 0.4)) %>% drop
+
+
 
 
 
