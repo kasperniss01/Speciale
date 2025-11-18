264d117b4780261500e53e92235085c629db2333
davidsenderovitz <128755629+davidsenderovitz@users.noreply.github.com>
2025-11-18 20:54:06 +0100

    iid gaussian T = 100-2410

--- sim_rej_rate_iid_level.R
+++ sim_rej_rate_iid_level.R
@@ -18,9 +18,11 @@ simulate_iid_gaussian_ts <- function(Tlen) {
 }
 
 
+B_func <- function(T) floor(T^(1/4))
+L = 10
+repetitions <- 200
 
-
-Tlens_iid <- c(100)
+Tlens_iid <- c(100,250,260,620,630,1290,1300,2400,2410)
 for(Tlen in Tlens_iid){
 
     print(paste0("Tlen = ", Tlen))
@@ -30,7 +32,8 @@ for(Tlen in Tlens_iid){
       L = L,
       B = B_func(Tlen),
       parameters = list(
-        "distribution" = simulate_iid_gaussian_ts
+        distribution = simulate_iid_gaussian_ts,
+        d = 3
       ),
       DGP = "IID",
       alphas = seq(0.005, 1, 0.005),
@@ -41,9 +44,6 @@ for(Tlen in Tlens_iid){
     sim_temp <- list(
       metadata = list(
         Tlen = Tlen,
-        baseline_gamma = baseline_gamma,
-        actual_gamma = gamma,
-        A_matrix = Alocal,
         B = B_func(Tlen),
         L = L,
         repetitions = repetitions
@@ -55,4 +55,22 @@ for(Tlen in Tlens_iid){
     
     
 }
+
+
+readRDS("datasets/local_alternatives_4D/sim_rej_rate_AR1_4D_local_alt_Tlen_100_baseline_gamma_0.rds")$sim_rej_obj$rejection_rate_df %>% ggplot(aes(x = alpha, y = rate_nonparametric)) + 
+  geom_line() +
+  geom_abline() + 
+  labs(title = paste0("Rejection Rate vs Alpha (Tlen = ", readRDS("datasets/local_alternatives_4D/sim_rej_rate_AR1_4D_local_alt_Tlen_100_baseline_gamma_0.rds")$metadata$Tlen, ")"),
+       x = "Alpha",
+       y = "Rejection Rate") +
+  theme_minimal()
+
+
+sim_temp$sim_rej_obj$rejection_rate_df %>% ggplot(aes(x = alpha, y = rate_nonparametric)) + 
+  geom_line() +
+  geom_abline() + 
+  labs(title = paste0("Rejection Rate vs Alpha (Tlen = ", sim_temp$metadata$Tlen, ")"),
+       x = "Alpha",
+       y = "Rejection Rate") +
+  theme_minimal()
 
