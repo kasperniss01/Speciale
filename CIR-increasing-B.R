9739c689f2a3311fe27bf1d778234d50f8206076
kasperniss01 <kasperniss@gmail.com>
2025-12-08 09:54:03 +0100

    CIR and VAR simulations, local alternatives and level

---
+++ CIR-increasing-B.R
@@ -0,0 +1,73 @@
+### CIR increasing B - level and local alternatives ### 
+
+source("sim_rejection_rate.R")
+source("simulate_parameters.R")
+
+#setup
+L <- 10
+repetitions <- 200
+B_func <- function(T) ceiling(T^(49 / 100))
+
+#Y is 3-dimensional
+d <- 3
+e <- 1/sqrt(d) * rep(1, d)
+
+#seeds for param generation and datasets
+seed <- 420
+seeds <- 1:200
+
+# Tlens <- c(seq(100, 1000, 100), 1000, 2000, 3000, 4000, 5000)
+Tlens <- c(100, 500, 1000, 2000)
+baseline_gammas <- c(0, 1, 10, 20, 30)
+
+#initial theta values
+theta_base <- CIR_param(d = d, seed = seed, gamma = 0) 
+
+for(Tlen in Tlens){
+  for(baseline_gamma in baseline_gammas){
+      
+      print(paste0("Tlen = ", Tlen, ", baseline gamma = ", baseline_gamma))
+      
+      gamma <- baseline_gamma / sqrt(Tlen)
+      
+      theta_base <- theta_base
+      theta2_local <- theta_base$theta2
+      theta2_local[1, -1] <- gamma * e
+      
+      theta_local <- list(theta1 = theta_base$theta1,
+                          theta2 = theta2_local,
+                          theta3 = theta_base$theta3)
+      
+      simulate_temp <- sim_rej_rate(
+        Tlen = Tlen,
+        L = L,
+        B = B_func(Tlen),
+        DGP = "CIR", 
+        parameters = list(
+          theta = theta_local
+        ),
+        alphas = seq(0.005, 1, 0.005),
+        repetitions = repetitions
+      )
+      
+      
+      sim_temp <- list(
+        metadata = list(
+          Tlen = Tlen,
+          baseline_gamma = baseline_gamma,
+          actual_gamma = gamma,
+          theta = theta_local, 
+          # A_matrix = Alocal,
+          B = B_func(Tlen),
+          L = L,
+          repetitions = repetitions
+        ),
+        sim_rej_obj = simulate_temp
+      )
+      
+      saveRDS(sim_temp, 
+              file = paste0("datasets/new_sims/CIR/_Tlen_", Tlen, "_baseline_gamma_", baseline_gamma, "_B_", B_func(Tlen), ".rds"))
+      
+    
+  }
+}
--- plot-VAR-results.R
+++ plot-VAR-results.R
@@ -160,7 +160,7 @@ local_alternative_VAR_fixed_B_alpha_0.05_plot <- local_alternative_VAR_fixed_B_d
   ylim(0, 1) +
   geom_point(size = 0.5) + 
   geom_abline(color = "darkgrey", linetype = "dashed") + 
-  facet_wrap(~baseline_gamma,
+  facet_wrap(~baseline_gamma + B,
              labeller = label_bquote(
                cols = gamma[0] == .(baseline_gamma) ~ "," ~ B == .(B)
              )) + 
@@ -171,3 +171,138 @@ local_alternative_VAR_fixed_B_alpha_0.05_plot
 
 
 ## Increasing B ##
+
+# Level #
+level_increasing_B_VAR_df <- comb_rej_rate_large_obj(
+  readRDS("datasets/new_sims/VAR/_Tlen_100_baseline_gamma_0_B_10.rds"),
+  readRDS("datasets/new_sims/VAR/_Tlen_500_baseline_gamma_0_B_22.rds"),
+  readRDS("datasets/new_sims/VAR/_Tlen_1000_baseline_gamma_0_B_30.rds")
+  # readRDS("datasets/new_sims/VAR/_Tlen_2000_baseline_gamma_0_B_42.rds")
+)
+
+level_increasing_B_plot <-  level_increasing_B_VAR_df %>% 
+  pivot_longer(cols = c(rate_nonparametric, rate_parametric_plugin, rate_oracle_plugin),
+               values_to = "rate",
+               names_to = "Method",
+               names_prefix = "rate_") %>% 
+  mutate(Method = ifelse(Method == "oracle_plugin", "Oracle", Method)) %>% 
+  mutate(Method = ifelse(Method == "parametric_plugin", "Parametric", Method)) %>% 
+  mutate(Method = ifelse(Method == "nonparametric", "Nonparametric", Method)) %>% 
+  ggplot(aes(x = alpha, y = rate, color = Method)) + 
+  labs(
+    x = expression(paste("Significance level (", alpha, ")")),
+    y = "Rejection rate"
+  ) + 
+  geom_line() + 
+  coord_fixed() + 
+  geom_abline(color = "grey", linetype = "dashed") + 
+  facet_wrap(~Tlen + B,
+             labeller = label_bquote(
+               cols = T == .(Tlen) ~ "," ~ B == .(B)
+             )) + 
+  theme_bw() + 
+  theme(strip.background = element_rect(fill = "white", color = NA),
+        legend.position = "bottom")
+
+ggsave("images/level_increasing_B_VAR.eps",
+       width = 500,
+       height = 250,
+       units = "px",
+       scale = 4,
+       plot = level_increasing_B_plot)
+
+
+# Local alternatives #
+local_alternative_VAR_increasing_B_df <- comb_rej_rate_large_obj(
+  readRDS("datasets/new_sims/VAR/_Tlen_100_baseline_gamma_3_B_10.rds"),
+  readRDS("datasets/new_sims/VAR/_Tlen_100_baseline_gamma_5_B_10.rds"),
+  # readRDS("datasets/new_sims/VAR/_Tlen_100_baseline_gamma_10_B_10.rds"),
+  # readRDS("datasets/new_sims/VAR/_Tlen_100_baseline_gamma_15_B_10.rds"),
+  readRDS("datasets/new_sims/VAR/_Tlen_500_baseline_gamma_3_B_22.rds"),
+  readRDS("datasets/new_sims/VAR/_Tlen_500_baseline_gamma_5_B_22.rds"),
+  readRDS("datasets/new_sims/VAR/_Tlen_500_baseline_gamma_10_B_22.rds"),
+  readRDS("datasets/new_sims/VAR/_Tlen_500_baseline_gamma_15_B_22.rds"),
+  readRDS("datasets/new_sims/VAR/_Tlen_1000_baseline_gamma_3_B_30.rds"),
+  readRDS("datasets/new_sims/VAR/_Tlen_1000_baseline_gamma_5_B_30.rds"),
+  readRDS("datasets/new_sims/VAR/_Tlen_1000_baseline_gamma_10_B_30.rds"),
+  readRDS("datasets/new_sims/VAR/_Tlen_1000_baseline_gamma_15_B_30.rds"),
+  readRDS("datasets/new_sims/VAR/_Tlen_2000_baseline_gamma_3_B_42.rds"),
+  readRDS("datasets/new_sims/VAR/_Tlen_2000_baseline_gamma_5_B_42.rds"),
+  readRDS("datasets/new_sims/VAR/_Tlen_2000_baseline_gamma_10_B_42.rds"),
+  readRDS("datasets/new_sims/VAR/_Tlen_2000_baseline_gamma_15_B_42.rds")
+# 
+#   readRDS("datasets/new_sims/VAR/_Tlen_500_baseline_gamma_3_B_10.rds"),
+#   readRDS("datasets/new_sims/VAR/L_10_Tlen_500_baseline_gamma_5_B_10.rds"),
+#   readRDS("datasets/new_sims/VAR/_Tlen_500_baseline_gamma_10_B_10.rds"),
+#   readRDS("datasets/new_sims/VAR/_Tlen_500_baseline_gamma_15_B_10.rds"),
+#   readRDS("datasets/new_sims/VAR/_Tlen_1000_baseline_gamma_3_B_10.rds"),
+#   readRDS("datasets/new_sims/VAR/L_10_Tlen_1000_baseline_gamma_5_B_10.rds"),
+#   readRDS("datasets/new_sims/VAR/_Tlen_1000_baseline_gamma_10_B_10.rds"),
+#   readRDS("datasets/new_sims/VAR/_Tlen_1000_baseline_gamma_15_B_10.rds"),
+#   readRDS("datasets/new_sims/VAR/_Tlen_2000_baseline_gamma_3_B_10.rds"),
+#   readRDS("datasets/new_sims/VAR/_Tlen_2000_baseline_gamma_5_B_10.rds"),
+#   readRDS("datasets/new_sims/VAR/_Tlen_2000_baseline_gamma_10_B_10.rds"),
+#   readRDS("datasets/new_sims/VAR/_Tlen_2000_baseline_gamma_15_B_10.rds")
+)
+
+#full plot for local alternative
+local_alternative_VAR_increasing_B_full_plot <- local_alternative_VAR_increasing_B_df %>% 
+  pivot_longer(cols = c(rate_nonparametric, rate_parametric_plugin, rate_oracle_plugin),
+               values_to = "rate",
+               names_to = "Method",
+               names_prefix = "rate_") %>% 
+  mutate(Method = ifelse(Method == "oracle_plugin", "Oracle", Method)) %>% 
+  mutate(Method = ifelse(Method == "parametric_plugin", "Parametric", Method)) %>% 
+  mutate(Method = ifelse(Method == "nonparametric", "Nonparametric", Method)) %>% 
+  ggplot(aes(x = alpha, y = rate, color = Method)) + 
+  geom_line() + 
+  geom_abline(color = "darkgrey", linetype = "dashed") + 
+  labs(
+    x = expression(paste("Significance level (", alpha, ")")),
+    y = "Rejection rate") +
+  ylim(0, 1) + 
+  facet_grid(baseline_gamma ~ Tlen + B,
+             labeller = label_bquote(
+               rows = gamma[0] == .(baseline_gamma), 
+               cols = T == .(Tlen) ~ "," ~ B == .(B)  
+             )) + 
+  theme_bw() + 
+  theme(
+    strip.background = element_rect(fill = "white", color = NA),
+    legend.position = "bottom")
+local_alternative_VAR_increasing_B_full_plot
+
+local_alternative_VAR_increasing_B_alpha_0.05_plot <- local_alternative_VAR_increasing_B_df %>% 
+  filter(0.0475 <= alpha & alpha <= 0.0525) %>% 
+  pivot_longer(cols = c(rate_nonparametric, rate_parametric_plugin, rate_oracle_plugin),
+               values_to = "rate",
+               names_to = "Method",
+               names_prefix = "rate_") %>% 
+  mutate(Method = ifelse(Method == "oracle_plugin", "Oracle", Method)) %>% 
+  mutate(Method = ifelse(Method == "parametric_plugin", "Parametric", Method)) %>% 
+  mutate(Method = ifelse(Method == "nonparametric", "Nonparametric", Method)) %>% 
+  ggplot(aes(x = Tlen, y = rate, color = Method)) + 
+  geom_line() + 
+  labs(x = "Tlen",
+       y = expression(paste("Estimated rejection rate (", alpha, " = 5%)"))) + 
+  ylim(0, 1) +
+  geom_point(size = 0.5) + 
+  geom_abline(color = "darkgrey", linetype = "dashed") + 
+  facet_wrap(~baseline_gamma,
+             labeller = label_bquote(
+               cols = gamma[0] == .(baseline_gamma) 
+             )) + 
+  theme_bw() + 
+  theme(strip.background = element_rect(fill = "white", color = NA),
+        legend.position = "bottom")
+local_alternative_VAR_increasing_B_alpha_0.05_plot
+
+ggsave("images/local_alternatives_increasing_B_VAR.eps",
+       width = 1000,
+       height = 500,
+       units = "px",
+       scale = 2.5,
+       plot = local_alternative_VAR_increasing_B_alpha_0.05_plot)
+
+
+
